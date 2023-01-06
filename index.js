47785dcfc7fb470ead63cc05a7a3b1670f24ec80
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-01-05 23:32:10 -0600

    added a script to be able to scroll up and down

---
+++ index.js
@@ -0,0 +1,9 @@
+let stars = document.getElementById('stars'),
+moon = document.getElementById('moon'),
+mountain_behind = document.getElementById('mountain_n=behind'),
+mountain_front = document.getElementById('mountain_front')
+text = document.getElementById('text'),
+btn = document.querySelector('.btn')
+
+
+
