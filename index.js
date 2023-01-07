1b12974a4a4d3421ee3fb7a855388876c795fca3
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-01-06 23:29:59 -0600

    fixed bug where the explore btn is visible while scrolling down, and that with adding a overflow hiden to the section

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
 let btn = document.querySelector('.btn');
 let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('.header');
 
 
 
@@ -15,5 +16,6 @@ window.addEventListener('scroll', function(){
   mountains_front.style.top= value * 0 + 'px';
   text.style.marginRight = value * 4 + 'px';
   text.style.marginTop = value * 1.5 + 'px';
-  btn.style.marginRight = value * 1.5 + 'px';
+  btn.style.marginTop = value * 1.5 + 'px';
+  header.style.top = value * 0.5+ 'px';
 });
